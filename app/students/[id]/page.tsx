7347bc175002358f241a5b935239201698d14ddc
Ryan Pang <ryanpang@MacBook-Air-3.local>
2025-08-13 19:17:49 -0400

    add generate static param

---
+++ app/students/[id]/page.tsx
@@ -0,0 +1,5 @@
+export async function generateStaticParams() {
+  // TODO: Fetch all student IDs from Firestore and return as:
+  // return [{ id: "student1" }, { id: "student2" }, ...];
+  return [];
+}
